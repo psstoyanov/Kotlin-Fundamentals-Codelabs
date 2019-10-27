cf7592fc4837b472df53d48d845b41e13049cab7
PakoSt <kokotekilata@gmail.com>
2019-10-27 21:40:27 +0000

    Roll Dice 01.2.3 - Minor cleanup - Change the name in the App Bar

--- app/src/main/java/com/example/kotlinfundamentalscodelab/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfundamentalscodelab/MainActivity.kt
@@ -13,21 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = getString(R.string.default_roll_text)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice(resultText) }
-        resultText.text = getString(R.string.default_roll_text)
 
     }
 
     private fun rollDice(resultText: TextView) {
         val randomInt = Random().nextInt(6) + 1
         resultText.text = randomInt.toString()
-
-        Toast.makeText(
-            this, "button clicked",
-            Toast.LENGTH_SHORT
-        ).show()
     }
 }
