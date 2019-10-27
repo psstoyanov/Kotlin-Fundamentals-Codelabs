75bbdaa94a16a97cf1349fef9a54a565b4dbe956
PakoSt <kokotekilata@gmail.com>
2019-10-27 21:34:20 +0000

    Roll Dice 01.2.2 - Set the default text - Set random number on press

--- app/src/main/java/com/example/kotlinfundamentalscodelab/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfundamentalscodelab/MainActivity.kt
@@ -3,20 +3,31 @@ package com.example.kotlinfundamentalscodelab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val resultText: TextView = findViewById(R.id.result_text)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener { rollDice(resultText) }
+        resultText.text = getString(R.string.default_roll_text)
+
     }
 
-    private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+    private fun rollDice(resultText: TextView) {
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
+
+        Toast.makeText(
+            this, "button clicked",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
